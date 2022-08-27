690a0b435ad7108b42361e042a9e03d75b5dda1d
dong ngo <ngodongdac3005@gmail.com>
2022-08-27 10:41:32 +0700

    Update pagination on movie page

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -1,23 +1,53 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import ReactPaginate from "react-paginate"
 import useSWR from "swr"
 import MovieCard from "../components/movie/MovieCard"
-import { fetcher } from "../config/config"
+import { API_KEY, fetcher } from "../config/config"
+import useDebounce from "../hooks/useDebounce"
+
+// search
+// https://api.themoviedb.org/3/search/movie?api_key=b2bfd7a8fd6001ec78e71956b1a29faa&language=en-US&page=1&include_adult=false
+
+const itemsPerPage = 20
+const pageCount = 5
 
 const MoviePage = () => {
-  const { data } = useSWR(
-    `https://api.themoviedb.org/3/movie/popular?api_key=b2bfd7a8fd6001ec78e71956b1a29faa`,
+  const [nextPage, setNextPage] = useState(1)
+  const [filter, setFilter] = useState('')
+  const [url, setUrl] = useState(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
+  const { data, error } = useSWR(
+    url,
     fetcher
   )
 
+  const loading = !data && !error
+
+  const filterDebounce = useDebounce(filter, 1000)
+
+  useEffect(() => {
+    if (filterDebounce) {
+      setUrl(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${filterDebounce}&page=${nextPage}`)
+    } else (
+      setUrl(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=${nextPage}`)
+    )
+  }, [filterDebounce, nextPage])
+
+  // if (!data) return null
   const movies = data?.results || []
+  // const { page, totalPage } = data
+
+  const handleChangeFilter = (e) => {
+    setFilter(e.target.value)
+  }
 
   return (
     <div className="page-container">
       <div className="flex gap-x-5 py-10">
         <input
           type="text"
-          className="w-full p-3 border rounded-lg outline-none "
+          className="w-full p-3 rounded-lg outline-none bg-slate-800 text-white "
           placeholder="Type here your search..."
+          onChange={handleChangeFilter}
         />
         <button className="text-white bg-primary p-3 rounded-lg">
           <svg
@@ -36,11 +66,35 @@ const MoviePage = () => {
           </svg>
         </button>
       </div>
-      <div className="grid grid-cols-4 gap-10">
-        {movies.length > 0 &&
-          movies.map((item) => {
-            return <MovieCard item={item} key={item.id} />
-          })}
+      {
+        loading ? (<div className="w-10 h-10 rounded-full border-4 border-t-transparent border-primary border-t-4 mx-auto animate-spin"></div>) : (
+          <div className="grid grid-cols-4 gap-10">
+            {movies.length > 0 &&
+              movies.map((item) => {
+                return <MovieCard item={item} key={item.id} />
+              })}
+          </div>
+        )
+      }
+      <div className="pagination flex justify-center items-center gap-x-5 my-10">
+        <span className="cursor-pointer">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+          </svg>
+        </span>
+        {
+          new Array(pageCount).fill(0).map((item, index) => {
+            return (
+              <span className="cursor-pointer px-2" key={index} onClick={() => setNextPage(index + 1)}>{index + 1}</span>
+            )
+          })
+        }
+
+        <span className="cursor-pointer">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+          </svg>
+        </span>
       </div>
     </div>
   )
